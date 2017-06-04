124a216301f16447e7b01e74ef8d2e3dec1142c3
PovilasID <povilasid@gmail.com>
2017-06-04 17:36:09 +0300

    Main page material

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -3,12 +3,31 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser, logoutUser } from '../actions/firebase_actions';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import HomeIcon from 'material-ui-icons/Home';
+import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import Divider from 'material-ui/Divider';
+import Grid from 'material-ui/Grid';
+
+import 'typeface-roboto'
+
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-
+        this.state = {
+            drawerOpen: false,
+            anchorEl: undefined,
+            userMenuOpen: false,
+        };
         this.props.fetchUser();
         this.logOut = this.logOut.bind(this);
     }
@@ -19,65 +38,92 @@ class App extends Component {
             this.props.fetchUser();
         });
     }
+      handleRequestClose () {
+        this.setState({ open: false });
+      };
 
     renderUserMenu(currentUser) {
     // if current user exists and user id exists than make user navigation
         if (currentUser && currentUser.uid) {
             return (
-                <li className="dropdown">
-                    <a
-                      href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
-                      aria-haspopup="true" aria-expanded="false"
-                    >
-                        {currentUser.email} <span className="caret" /></a>
-                    <ul className="dropdown-menu">
-                        <li><Link to="/profile">Profile</Link></li>
-                        <li role="separator" className="divider" />
-                        <li><Link to="/logout" onClick={this.logOut}>Logout</Link></li>
-                    </ul>
-                </li>
+                <div>
+                <Button 
+                    aria-owns="simple-menu" 
+                    aria-haspopup="true" 
+                    onClick={(event) =>  this.setState({ userMenuOpen: true, anchorEl: event.currentTarget })} 
+                    contrast>
+                  {currentUser.email}
+                </Button>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={this.state.anchorEl}
+                  open={this.state.userMenuOpen}
+                  onRequestClose={()=>this.setState({ userMenuOpen: false })}
+                >
+                  <MenuItem onClick={()=>this.setState({ userMenuOpen: false })}>
+                    <Link to="/profile">Profile</Link>
+                  </MenuItem>
+                  <MenuItem onClick={()=>this.setState({ userMenuOpen: false })}>
+                    <Link to="/logout" onClick={this.logOut}>Logout</Link>
+                  </MenuItem>
+                </Menu>
+                </div>
             );
         } else {
-            return [
-                <li key={1}><Link to="/login">Login</Link></li>,
-                <li key={2}><Link to="/register">Register</Link></li>,
-            ];
+            return (
+                <div>
+                    <Link to="/login"><Button to="/login" contrast>Login</Button></Link>
+                    <Link to="/register"><Button contrast>Register</Button></Link>
+                </div>
+            );
         }
     }
 
+    handleLeftClose = () => this.toggleDrawer('left', false);
+
     render() {
+        const sideList = (
+          <div>
+            <List disablePadding>
+            <div>
+                <Link to="/">
+                    <ListItem button>
+                      <ListItemIcon>
+                        <HomeIcon />
+                      </ListItemIcon>
+                      <ListItemText primary="Home" />
+                    </ListItem>
+                </Link>
+            </div>
+            </List>
+          </div>
+        );
         return (
             <div>
-                <header className="navbar navbar-static-top navbar-inverse" id="top" role="banner">
-                    <div className="container">
-                        <div className="navbar-header">
-                            <button
-                              className="navbar-toggle collapsed" type="button" data-toggle="collapse"
-                              data-target=".bs-navbar-collapse"
-                            ><span className="sr-only">Toggle navigation</span>
-                                <span className="icon-bar" />
-                                <span className="icon-bar" />
-                                <span className="icon-bar" />
-                            </button>
-                            <Link to="/" className="navbar-brand">Firebase & Redux boilerplate</Link>
-
-                        </div>
-                        <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-                            <ul className="nav navbar-nav">
-                                <li><Link to="/"> Home</Link></li>
-                ,
-              </ul>
-                            <ul className="nav navbar-nav navbar-right">
-                                { this.renderUserMenu(this.props.currentUser) }
-                            </ul>
-                        </nav>
-                    </div>
-                </header>
-
-                <div className="container">
+                <AppBar>
+                    <Toolbar>
+                      <IconButton contrast>
+                        <MenuIcon onClick={() => this.setState({drawerOpen: true})} />
+                      </IconButton>
+                        <Typography type="title" colorInherit>
+                            Firebase & Redux boilerplate
+                        </Typography>
+                        { this.renderUserMenu(this.props.currentUser) }
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                  open={this.state.drawerOpen}
+                  onRequestClose={() => this.setState({drawerOpen: false})}
+                  onClick={() => this.setState({drawerOpen: false})}
+                >
+                {sideList}
+                </Drawer>
+                <Grid container gutter={24}>
+                    <Grid item xs={12}>
                     {this.props.children}
-                </div>
-            </div>
+                    </Grid>
+                </Grid>            
+        </div>
         );
     }
 }
